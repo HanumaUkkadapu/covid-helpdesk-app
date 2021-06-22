82cc54630d3a52e8b5131ef0c06547ccbd38f174
Tulasi Veera Hanuma Ukkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2021-06-22 10:36:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let root, body, menuBtn, navEl, navLinks;
+
+window.addEventListener("DOMContentLoaded", () => {
+	init();
+});
+
+function init(){
+    root = document.documentElement;
+	body = document.body;
+    menuBtn = document.getElementById('menu');
+    navEl = document.getElementById('nav_el');
+    navLinks = document.querySelectorAll("#nav_el a");
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle("open");
+        navEl.classList.toggle('open');
+		body.classList.toggle("menu-open");
+    });
+    
+    navLinks.forEach((el) => {
+        el.addEventListener("click", () => {
+            if (menuBtn.classList.contains("open")) menuBtn.click();
+        });
+    });
+}
