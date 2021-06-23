cefd3edfb545f6c8d5e13e7cd36de83e6b656d95
Tulasi Veera Hanuma Ukkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2021-06-23 15:57:22 +0530

    Update_1
    Added Search Section.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function init(){
     menuBtn = document.getElementById('menu');
     navEl = document.getElementById('nav_el');
     navLinks = document.querySelectorAll("#nav_el a");
-
+    
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle("open");
         navEl.classList.toggle('open');
@@ -21,5 +21,25 @@ function init(){
         el.addEventListener("click", () => {
             if (menuBtn.classList.contains("open")) menuBtn.click();
         });
+    });
+
+    /* Hanuma */
+    typeCheckIns = document.querySelectorAll(".inWrap input[type='checkbox']");
+    addSVGChecks();
+
+}
+
+
+/* Hanuma */
+
+let typeChkIns;
+
+function addSVGChecks(){
+    let svgCheck = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 100 100" version="1.1" >
+<path d="m 15 55 l 20 20 l 50 -50"
+     fill="none" stroke-width="10" />
+</svg>`;
+    typeCheckIns.forEach((el)=>{
+        el.insertAdjacentHTML('afterend',svgCheck);
     });
 }
